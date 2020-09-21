336205613709e26f7caa7604aa94de8accff3eee
kosta-design <66972581+kosta-design@users.noreply.github.com>
2020-09-21 18:08:29 +0200

    Add files via upload

---
+++ kosta_loading-dragon/__resource.lua
@@ -0,0 +1,25 @@
+files {
+    'index.html',
+    'css/back.css',
+    'css/img.css',
+    'css/menu.css',
+    'css/musicbutton.css',
+    'css/cursor.css',
+    'img/blush.png',
+    'img/body.png',
+    'img/eye.png',
+    'img/fire.png',
+    'img/full.jpg',
+    'img/horn-left.png',
+    'img/horn-right.png',
+    'img/mouth.png',
+    'img/tail-sting.png',
+    'img/sloroyale-logo.png',
+    'js/main.js',
+    'js/plugins.js',
+    'audio.mp3',
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
